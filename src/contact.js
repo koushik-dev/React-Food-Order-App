9e063077e25f4eb19df9d6537124a5bbec884eed
koushik <koushik.ramaswamy@apitive.com>
2020-03-26 13:21:16 +0530

    app update

---
+++ src/contact.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './contact.css';
+
+function Contact() {
+    const [formValue, setValue] = useState({ });
+    function valueChange(event) {
+        this.setValue({...this.formValue, name: event.target.value})
+    }
+    return (
+      <div className="contact-container">
+        <form className="flex">
+          <label className="flex">
+            Name
+            <input type="text" onChange={event => setValue({ ...formValue, name: event.target.value})} />
+          </label>
+          <label className="flex">
+            Contact
+            <input type="text" onChange={event => setValue({ ...formValue, contact: event.target.value})} />
+          </label>
+          <label className="flex">Message<textarea rows="5" onChange={event => setValue({ ...formValue, message: event.target.value })}></textarea></label>
+          <input className="submit" type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+}
+
+export default Contact;
