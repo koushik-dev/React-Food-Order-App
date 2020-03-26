3b7cbb61e61dc3b89330939344b5834c04b717a6
koushik <koushik.ramaswamy@apitive.com>
2020-03-26 13:21:11 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import logo from './logo.png'
 import './App.css';
+
+import Home from './home';
+import Order from './order';
+import Contact from './contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <Router>
+        <nav id="nav" className="flex nav">
+          <h1><Link to="/"><img className="logo" src={logo} alt="logo" /></Link></h1>
+          <ul className="flex">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/order">Order</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>       
+        </nav>
+        <div className="viewport">
+          <Switch>
+            <Route path="/contact">
+              <Contact/>
+            </Route>
+            <Route path="/order">
+              <Order/>
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+        
+      </Router>
     </div>
   );
 }
